a4ffe095bd64ab5d8453dab00d2a6a57b5a39582
Ryo <rrblx511@gmail.com>
2026-02-26 21:31:43 +0100

    Update string_ptr.hpp

--- include/hooks/debug_menu/string_ptr.hpp
+++ include/hooks/debug_menu/string_ptr.hpp
@@ -2,8 +2,10 @@
 #include <cstdlib>
 #include "libg/functions.hpp"
 
-namespace hooks::debug_menu {
-    inline void* makeScPtr(const char* str) {
+namespace hooks::debug_menu 
+{
+    inline void* makeScPtr(const char* str) 
+    {
         void* mem = malloc(40);
         libg::functions::stringctor(mem, str);
         return mem;
